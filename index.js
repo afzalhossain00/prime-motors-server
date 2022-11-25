9251b5018c5374ecfdd46d77afb81458d6181563
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-26 00:55:05 +0600

    database connention setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -8,6 +9,12 @@ const app = express()
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqk1vsl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', async (req, res) => {
     res.send('Prime Motors server is running')
