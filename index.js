31888dc3c12f70a4543a36873c4515d475aa1551
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-26 12:09:26 +0600

    load category item collection

--- index.js
+++ index.js
@@ -15,6 +15,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoryOptionCollction = client.db('primeMotors').collection('category')
+        const bikesOptionCollction = client.db('primeMotors').collection('bikes')
+
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const options = await categoryOptionCollction.find(query).toArray();
+            res.send(options);
+        })
+
+        app.get('/category/:categoryId', async (req, res) => {
+            const categoryId = req.params.categoryId;
+            const query = { categoryId: parseInt(categoryId) }
+            const result = await bikesOptionCollction.find(query).toArray()
+            res.send(result);
+        })
+    }
+
+    finally {
+
+    }
+
+}
+
+run().catch(console.log);
+
 
 app.get('/', async (req, res) => {
     res.send('Prime Motors server is running')
